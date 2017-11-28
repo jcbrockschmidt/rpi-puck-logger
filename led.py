8b2836d9c0352ea6dd9724ebe9fc7d5a10fd64ed
Joshua Brockschmidt <joshuabrockschmidt@gmail.com>
2017-11-28 15:16:38 -0800

    Add LED code

---
+++ led.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+PIN = 8
+DELAY = 1
+
+import sys
+
+# Check arguments
+if len(sys.argv) <= 1:
+    print("Please provide an argument")
+    print("Usage: {} on|off|blink".format(sys.argv[0]))
+    sys.exit(1)
+
+    
+import RPi.GPIO as GPIO
+import signal
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIN, GPIO.OUT)
+
+def cancelBlink():
+    print("Cancelling LED blinking")
+    GPIO.output(PIN, GPIO.LOW)
+    GPIO.cleanup()
+
+def catchTerm(signal, frame):
+    # Called when SIGTERM is detected
+    cancelBlink()
+    sys.exit(0)
+
+if sys.argv[1] == "on":
+    print("Turning LED on")
+    GPIO.output(PIN, GPIO.HIGH)
+
+elif sys.argv[1] == "off":
+    print("Turning LED off")
+    GPIO.output(PIN, GPIO.LOW)
+    GPIO.cleanup()
+
+elif sys.argv[1] == "blink":
+    print("Blinking LED")
+    signal.signal(signal.SIGTERM, catchTerm)
+    try:
+        while True:
+            GPIO.output(PIN, True)
+            time.sleep(DELAY)
+            GPIO.output(PIN, False)
+            time.sleep(DELAY)
+    except KeyboardInterrupt:
+        cancelBlink()
+
+else:
+    print("Invalid argument: {}".format(sys.argv[1]))
