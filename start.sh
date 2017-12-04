a5e56a59a353f608d1f8c75cb3bbab9e9fdcba63
Joshua Brockschmidt <joshuabrockschmidt@gmail.com>
2017-12-04 15:12:30 -0800

    Use LED to indicate capturing status
    Blink when packets can not be captured. Turn on when packets are being
    captured successfully.

--- start.sh
+++ start.sh
@@ -1,43 +1,66 @@
 #!/usr/bin/env bash
 
+TMPDIR="/tmp/rpipucklogger"
+TMPBLK="$TMPDIR/blink.pid"
+# Starts LED blinking
+blink () {
+    # Kill blinking process if it exists
+    if [[ -e $TMPBLK ]]
+    then
+	BLKPID=$(cat $TMPBLK)
+	PINFO=$(ps -f -p $BLKPID)
+	if [[ $PINFO == *"led.py"* ]]
+	then
+	    kill $BLKPID
+	fi
+    fi
+    $DIR/led.py blink &
+    echo $! > $TMPBLK
+}
+
 # Make sure script is being run as sudo
 if [[ $UID != 0 ]]
 then
-	echo "Please run this script as root"
-	exit 1
+    echo "Please run this script as root"
+    blink
+    exit 1
 fi
 
 # Make sure an interface is specified
 INTER=$1
 if [ -z $INTER ]
 then
-	echo "Please specify an interface to capture packets from (e.g. eth0)"
-	exit 1
+    echo "Please specify an interface to capture packets from (e.g. eth0)"
+    blink
+    exit 1
 fi
 
 # Make sure interface is up
 ip l set $INTER up
 if [ $? -ne 0 ]
 then
-	echo "Error with $INTER detected"
-	exit 1
+    echo "Error with $INTER detected"
+    blink
+    exit 1
 fi
 
 # Test if interface is capturing packets
+DIR=$(dirname "$(readlink -f "$0")")
+mkdir -p $TMPDIR
 DATE=$(date +"%F_%k%M%S")
-TMP="/tmp/puck_$DATE.pcap"
-timeout 5s tcpdump -i $INTER -w $TMP
-NUMPACKS=$(tcpdump -r $TMP 2> /dev/null | wc -l)
-rm -f $TMP
+TEST="$TMPDIR/puck_$DATE.pcap"
+timeout 5s tcpdump -i $INTER -w $TEST
+NUMPACKS=$(tcpdump -r $TEST 2> /dev/null | wc -l)
+rm -f $TEST
 if [ $NUMPACKS -lt 1 ]
 then
-	echo "No packets are being captured through $INTER"
-	# TODO: blink LED until poweroff
-	exit 1
+    echo "No packets are being captured through $INTER"
+    blink
+    exit 1
 fi
 
 # Capture packets and write to $DUMP
-DIR=$(dirname "$(readlink -f "$0")")
 DUMP="$DIR/$DATE.pcap"
 echo "Recording packets from $INTER and writing to $DUMP"
-tcpdump -w $DUMP -i $INTER # TODO: turn LED on
+$DIR/led.py on
+tcpdump -w $DUMP -i $INTER
